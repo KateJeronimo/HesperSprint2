1304889537d8b4bda45ac7948357bbacef4fff52
daniel <dano_la@yahoo.com>
2023-03-01 18:45:34 -0500

    Initial Framework for Sprint 2

---
+++ src/test/java/com/cloud/pages/LoginPage.java
@@ -0,0 +1,21 @@
+package com.cloud.pages;
+
+
+import com.cloud.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    public LoginPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+
+
+
+
+    }
+
+
