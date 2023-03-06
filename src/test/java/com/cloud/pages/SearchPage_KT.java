7bc54425c7abb9b65e9fb7b2bd3a9cb620d2426f
KateJeronimo <ekate.trusova@gmail.com>
2023-03-06 17:39:01 -0500

    Kate's update

--- src/test/java/com/cloud/pages/SearchPage_KT.java
+++ src/test/java/com/cloud/pages/SearchPage_KT.java
@@ -1,6 +1,28 @@
 package com.cloud.pages;
 
+import com.cloud.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class SearchPage_KT {
 
     // Kate's user story
+
+    public SearchPage_KT(){
+
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+
+    @FindBy(xpath = "//div[@class='header-menu unified-search']")
+    public WebElement searchIcon;
+
+    @FindBy(xpath = "//span[@class='innernametext']")
+    public WebElement fileName;
+
+    @FindBy(xpath = "//input[@type='search']")
+    public WebElement searchBox;
+
+
 }
