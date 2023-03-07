91fbc1968dea875730461ef75eb44b7ab3b77995
KateJeronimo <ekate.trusova@gmail.com>
2023-03-07 15:39:37 -0500

    KT - finished US13, added userName, userPassword and website URL to configuration.properties

--- src/test/java/com/cloud/step_definitions/US13_StepDefs.java
+++ src/test/java/com/cloud/step_definitions/US13_StepDefs.java
@@ -1,13 +1,27 @@
 package com.cloud.step_definitions;
 
+import com.cloud.pages.LoginPage;
 import com.cloud.pages.SearchPage_KT;
+import com.cloud.utilities.BrowserUtils;
+import com.cloud.utilities.ConfigurationReader;
 import com.cloud.utilities.Driver;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.Keys;
 
 public class US13_StepDefs {
 
     SearchPage_KT searchPage_kt = new SearchPage_KT();
+    LoginPage loginPage = new LoginPage();
+
+
+    @Given("user is logged in with valid credentials")
+    public void user_Is_Logged_In_With_Valid_Credentials() {
+        Driver.getDriver().get(ConfigurationReader.getProperty("website"));
+        loginPage.login(ConfigurationReader.getProperty("userName"),ConfigurationReader.getProperty("userPassword"));
+    }
 
 
     @When("user is on Files page")
@@ -15,20 +29,31 @@ public class US13_StepDefs {
         Driver.getDriver().get("https://qa.trycloud.net/index.php/apps/files/?dir=/&fileid=54952");
     }
 
+
     @When("user clicks on the Search icon")
     public void user_clicks_on_the_search_icon() {
         searchPage_kt.searchIcon.click();
+        BrowserUtils.sleep(2);
     }
+
 
     @When("user searches for one of the files from Files page")
     public void user_searches_for_one_of_the_files_from_files_page() {
         String itemToSearch = searchPage_kt.fileName.getText();
-        searchPage_kt.searchIcon.sendKeys(itemToSearch);
+        searchPage_kt.searchBox.sendKeys(itemToSearch);
+        BrowserUtils.sleep(3);
+
     }
+
 
     @Then("user should see the file in the search results")
     public void user_should_see_the_file_in_the_search_results() {
 
-        System.out.println("in progress");
+        String searchItem = searchPage_kt.fileName.getText();
+
+        Assert.assertEquals(searchItem, searchPage_kt.result.getText());
+
     }
+
+
 }
