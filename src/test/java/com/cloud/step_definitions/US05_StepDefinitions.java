915cd52f0eaeb1eb81384c70aef656c5c9089181
Toma028 <118287514+Toma028@users.noreply.github.com>
2023-03-06 19:35:31 -0500

    US05 Step Defs

--- src/test/java/com/cloud/step_definitions/US05_StepDefinitions.java
+++ src/test/java/com/cloud/step_definitions/US05_StepDefinitions.java
@@ -1,6 +1,40 @@
 package com.cloud.step_definitions;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class US05_StepDefinitions {
+
+    @Given("the user is logged in")
+    public void the_user_is_logged_in() {
+
+    }
+
+    @Given("the user is on the {string} page")
+    public void the_user_is_on_the_page(String string) {
+
+    }
+
+    @When("the user selects a file")
+    public void the_user_selects_a_file() {
+
+    }
+
+    @When("clicks the {string} button")
+    public void clicks_the_button(String string) {
+
+    }
+
+    @Then("the file should be added to the user's favorites list")
+    public void the_file_should_be_added_to_the_user_s_favorites_list() {
+
+    }
+
+    @Then("the user should see a confirmation message")
+    public void the_user_should_see_a_confirmation_message() {
+
+    }
 
 
 }
