7a048f2749512b54100e785677f9e0b3654d1ead
daniel <dano_la@yahoo.com>
2023-03-06 22:20:06 -0500

    updated to correct user name & password

--- src/test/java/com/cloud/pages/LoginPage.java
+++ src/test/java/com/cloud/pages/LoginPage.java
@@ -38,8 +38,8 @@ public class LoginPage {
     public WebElement showPasswordIconBtn;
 
     public void login(){
-        this.inputUsernameOrEmail.sendKeys("User10");
-        this.inputPassword.sendKeys("UserPass123");
+        this.inputUsernameOrEmail.sendKeys("User199");
+        this.inputPassword.sendKeys("Userpass123");
         this.logInBtn.click();
     }
 
