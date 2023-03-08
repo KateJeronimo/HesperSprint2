48ac0ff205e20d13e49e6f01733b9cd8f6f9dffd
chingizaliev <chingiz_geo@yahoo.com>
2023-03-08 14:58:04 -0500

    last push

--- src/test/java/com/cloud/step_definitions/US06_stepDefinitions.java
+++ src/test/java/com/cloud/step_definitions/US06_stepDefinitions.java
@@ -10,27 +10,27 @@ public class US06_stepDefinitions {
     LoginPage loginPage = new LoginPage();
 
     FavoritesFiles_CA favoritesFiles_ca = new FavoritesFiles_CA();
-    @Given("user on the dashboard page")
+    @Given("User on the dashboard page")
     public void user_on_the_dashboard_page() {
-        Driver.getDriver().get("http://qa3.trycloud.net/index.php/login?clear=1");
-        loginPage.login();
+      Driver.getDriver().get("http://qa3.trycloud.net/index.php/login?clear=1");
+      loginPage.login();
 
     }
 
 
     @When("the user clicks the {string} module")
     public void the_user_clicks_the_module(String string) {
-     favoritesFiles_ca.files.click();
+    favoritesFiles_ca.files.click();
 
     }
 
     @When("the users click action-icon from any file on the page to remove")
     public void the_users_click_action_icon_from_any_file_on_the_page_to_remove() {
-     favoritesFiles_ca.actionIcon.click();
+    favoritesFiles_ca.actionIcon.isDisplayed();
     }
 
     @When("user choose the {string} option")
-    public void user_choose_the_option(String string) {
+    public void user_choose_the_option(String favorite) {
       //favoritesFiles_ca.addToFavorites.;
 
     }
--- src/test/resources/features/US06_favorites.feature
+++ src/test/resources/features/US06_favorites.feature
@@ -2,9 +2,9 @@
 Feature: As a user, I should be able to remove files from favorites and upload a file directly
 
   Story: As a user, I should be able to edit favorites files.
-@smoke
+
   Scenario: verify users to remove files to Favorites
-    Given user on the dashboard page
+    Given User on the dashboard page
     When the user clicks the "Files" module
     When the users click action-icon from any file on the page to remove
     And user choose the "Remove from favorites" option
