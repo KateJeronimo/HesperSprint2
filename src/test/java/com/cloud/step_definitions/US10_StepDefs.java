1a11361cc7ad06d61af707fd3ec9115caa9ed76c
Aprill <aprill.onq@gmail.com>
2023-03-08 17:49:14 -0600

    Updated all pages for US10

--- src/test/java/com/cloud/step_definitions/US10_StepDefs.java
+++ src/test/java/com/cloud/step_definitions/US10_StepDefs.java
@@ -1,39 +1,61 @@
 package com.cloud.step_definitions;
 
+import com.cloud.pages.DeleteFilesAndFolders_AKS;
+import com.cloud.pages.LoginPage;
+import com.cloud.utilities.BrowserUtils;
+import com.cloud.utilities.Driver;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 public class US10_StepDefs {
 
+    LoginPage loginPage = new LoginPage();
+
+    DeleteFilesAndFolders_AKS deleteFilesAndFolders_aks = new DeleteFilesAndFolders_AKS();
+
+    WebElement firstFolder = Driver.getDriver().findElement(By.xpath("(//td[@class='filename ui-draggable ui-draggable-handle'])[1]"));
+
+    String fileName = Driver.getDriver().findElement((By) firstFolder).getText();
+
     @Given("when the user is on the dashboard page")
     public void when_the_user_is_on_the_dashboard_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
+       loginPage.login();
     }
-    @When("the user clicks on the {string} module on the top of the screen")
-    public void the_user_clicks_on_the_module_on_the_top_of_the_screen(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    @When("the user clicks on the Files module on the top of the screen")
+    public void the_user_clicks_on_the_Files_module_on_the_top_of_the_screen() {
+
+        deleteFilesAndFolders_aks.filesPage.click();
+        BrowserUtils.sleep(3);
     }
-    @When("the user clicks the action-icon  from any file or folder on the right of the page")
+    @And("the user clicks the action-icon  from any file or folder on the right of the page")
     public void the_user_clicks_the_action_icon_from_any_file_or_folder_on_the_right_of_the_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
+        deleteFilesAndFolders_aks.actionIcon.click();
+        BrowserUtils.sleep(3);
     }
-    @When("the user chooses the {string} option")
-    public void the_user_chooses_the_option(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    @And("the user chooses the Delete File option")
+    public void the_user_chooses_the_Delete_File_option() {
+
+        deleteFilesAndFolders_aks.deleteFolder.click();
+        BrowserUtils.sleep(3);
     }
-    @When("the user clicks the {string} sub-module on the bottom left of the screen")
-    public void the_user_clicks_the_sub_module_on_the_bottom_left_of_the_screen(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    @When("the user clicks the Deleted Files sub-module on the bottom left of the screen")
+    public void the_user_clicks_the_Deleted_Files_sub_module_on_the_bottom_left_of_the_screen() {
+
+        deleteFilesAndFolders_aks.deletedFilesPage.click();
+        BrowserUtils.sleep(5);
     }
     @Then("verify the deleted file is displayed on the page")
     public void verify_the_deleted_file_is_displayed_on_the_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
+      // String deletedFolder = DeleteFilesAndFolders_AKS.folderName.getText();
+
+      Assert.assertTrue(fileName, firstFolder.isDisplayed());
     }
 }
--- src/test/resources/features/US10_DeleteFilesAndFolders.feature
+++ src/test/resources/features/US10_DeleteFilesAndFolders.feature
@@ -1,11 +1,11 @@
 
-Feature:  As a user, I should be able to update settings
+Feature:  As a user, I should be able to delete files/folders
 
   Scenario:  Verify user can delete files/folders
 
   Given when the user is on the dashboard page
-  When the user clicks on the "Files" module on the top of the screen
+  When the user clicks on the Files module on the top of the screen
   And the user clicks the action-icon  from any file or folder on the right of the page
-  And the user chooses the "Delete File" option
-  When the user clicks the "Deleted Files" sub-module on the bottom left of the screen
+  And the user chooses the Delete File option
+  When the user clicks the Deleted Files sub-module on the bottom left of the screen
   Then verify the deleted file is displayed on the page
