86b640fc64301990920610f3c54fef578ec56764
franco_Cydeo <frankiejknowsbest@Icloud.com>
2023-03-08 16:39:46 -0500

    these two files are working fine now only need to work on pages

--- src/test/java/com/cloud/step_definitions/US12_StepDefs.java
+++ src/test/java/com/cloud/step_definitions/US12_StepDefs.java
@@ -1,4 +1,32 @@
 package com.cloud.step_definitions;
 
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class US12_StepDefs {
+    @Given("user on the dashboard page")
+    public void user_on_the_dashboard_page() {
+
+    }
+
+    @When("the user clicks the {string} module")
+    public void the_user_clicks_the_module(String string) {
+
+    }
+
+
+    @And("user clicks Settings on the left bottom corner")
+    public void user_clicks_settings_on_the_left_bottom_corner() {
+
+    }
+    @Then("the user should be able to click any buttons")
+    public void the_user_should_be_able_to_click_any_buttons() {
+
+    }
+
+
+
+
 }
