f6e9ac0a073784b16dc21de56c28344dce843f2c
Aprill <aprill.onq@gmail.com>
2023-03-08 13:59:33 -0600

    Created step defs from Feature page

--- src/test/java/com/cloud/step_definitions/US10_StepDefs.java
+++ src/test/java/com/cloud/step_definitions/US10_StepDefs.java
@@ -1,4 +1,39 @@
 package com.cloud.step_definitions;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class US10_StepDefs {
+
+    @Given("when the user is on the dashboard page")
+    public void when_the_user_is_on_the_dashboard_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("the user clicks on the {string} module on the top of the screen")
+    public void the_user_clicks_on_the_module_on_the_top_of_the_screen(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("the user clicks the action-icon  from any file or folder on the right of the page")
+    public void the_user_clicks_the_action_icon_from_any_file_or_folder_on_the_right_of_the_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("the user chooses the {string} option")
+    public void the_user_chooses_the_option(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("the user clicks the {string} sub-module on the bottom left of the screen")
+    public void the_user_clicks_the_sub_module_on_the_bottom_left_of_the_screen(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("verify the deleted file is displayed on the page")
+    public void verify_the_deleted_file_is_displayed_on_the_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 }
