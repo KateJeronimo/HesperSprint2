c1d8d765f6a2e51e84a5cec85e0a75cab57cca0b
franco_Cydeo <frankiejknowsbest@Icloud.com>
2023-03-08 22:17:51 -0500

    ran my test and everything resulted well

--- src/test/java/com/cloud/pages/MainModulesPage_FJ.java
+++ src/test/java/com/cloud/pages/MainModulesPage_FJ.java
@@ -1,4 +1,24 @@
 package com.cloud.pages;
 
+import com.cloud.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class MainModulesPage_FJ {
+    public MainModulesPage_FJ(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath = "//header/div[1]/ul[1]/li[2]/a[1]")
+    public WebElement clicksFile;
+
+    @FindBy(xpath = "//button[contains(text(),'Settings')]")
+    public WebElement clicksSetting;
+
+    @FindBy(xpath = "//label[contains(text(),'Show rich workspaces')]")
+    public WebElement clickCheckbox;
+
+
+
 }
--- src/test/java/com/cloud/step_definitions/US12_StepDefs.java
+++ src/test/java/com/cloud/step_definitions/US12_StepDefs.java
@@ -1,28 +1,42 @@
 package com.cloud.step_definitions;
 
+import com.cloud.pages.LoginPage;
+import com.cloud.pages.MainModulesPage_FJ;
+import com.cloud.utilities.Driver;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class US12_StepDefs {
+
+    LoginPage loginPage = new LoginPage();
+    MainModulesPage_FJ mainModulesPage_FJ = new MainModulesPage_FJ();
+
     @Given("user on the dashboard page")
     public void user_on_the_dashboard_page() {
+        loginPage.inputUsernameOrEmail.sendKeys("user34");
+        loginPage.inputPassword.sendKeys("Userpass123");
+        loginPage.logInBtn.click();
 
     }
 
     @When("the user clicks the {string} module")
     public void the_user_clicks_the_module(String string) {
 
+        mainModulesPage_FJ.clicksFile.click();
     }
-
 
     @And("user clicks Settings on the left bottom corner")
     public void user_clicks_settings_on_the_left_bottom_corner() {
+        mainModulesPage_FJ.clicksSetting.click();
 
     }
     @Then("the user should be able to click any buttons")
     public void the_user_should_be_able_to_click_any_buttons() {
+        mainModulesPage_FJ.clickCheckbox.click();
 
     }
 
