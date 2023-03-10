26c0b8b17cb2023bba751bd33240142fc5deeab8
Toma028 <118287514+Toma028@users.noreply.github.com>
2023-03-10 13:10:05 -0500

    US05 Step Defs update

--- src/test/java/com/cloud/step_definitions/US05_StepDefinitions.java
+++ src/test/java/com/cloud/step_definitions/US05_StepDefinitions.java
@@ -6,13 +6,31 @@ import com.cloud.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+
+import java.util.Collection;
 
 public class US05_StepDefinitions {
 
 
+    public class User {
+        public Collection<Object> addFileToFavorites() {
+            return null;
+        }
+    }
+
+    private class AddFileToFavorites {
+    }
+
     AddFileToFavorites_Page addFileToFavorites_page = new AddFileToFavorites_Page();
 
     LoginPage loginPage = new LoginPage();
+    
+
+    AddFileToFavorites addFileToFavorites = new AddFileToFavorites();
+    User user = new User();
+
 
     @Given("the user is logged in")
     public void the_user_is_logged_in() {
@@ -26,7 +44,6 @@ public class US05_StepDefinitions {
     }
 
      */
-
     @When("the user clicks action-icon from any file on the page")
     public void theUserClicksActionIconFromAnyFileOnThePage() {
         //addFileToFavorites_page.actionIcon.click();
@@ -39,16 +56,15 @@ public class US05_StepDefinitions {
     }
     @Then("the file should be added to the user's favorites list")
     public void the_file_should_be_added_to_the_user_s_favorites_list() {
-        //addFileToFavorites_page.
 
+        Object files = null;
+        Assert.assertTrue(user.addFileToFavorites().contains(files));
 
     }
-
     @Then("the user should see a confirmation message")
     public void the_user_should_see_a_confirmation_message() {
 
+
     }
-
-
 
 }
